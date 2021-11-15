583eddb9ce3e04f2ebe3a714b0dbcce428e45a08
Bryan Molina <65988588+mv181962@users.noreply.github.com>
2021-11-15 00:54:54 -0600

    Recuperación parcial de avance por error en Git
    Esto es lo que se ha recuperado, luego que nuestro Git diera error y regresara nuestro proyecto de Netbeans a la última subida, que fue la noche anterior:
    
    Lo que hemos logrado rescatar es lo siguiente de progreso:
    
    - CRUD: Sí elimina (y recarga la página, también hace Soft Delete), sí enlista, sí crea (y redirige) - Edit no logramos que funcionara

--- src/java/org/fusalmo/www/managed_beans/MantenimientosBean.java
+++ src/java/org/fusalmo/www/managed_beans/MantenimientosBean.java
@@ -4,6 +4,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import java.util.List;
 import org.fusalmo.www.entities.MantenimientosEntity;
+import org.fusalmo.www.entities.RecursosEntity;
 import org.fusalmo.www.model.MantenimientosModel;
 import org.fusalmo.www.utils.JPAUtil;
 import org.fusalmo.www.utils.JsfUtil;
@@ -13,6 +14,15 @@ import org.fusalmo.www.utils.JsfUtil;
 public class MantenimientosBean {
     
     private String idRecurso;
+    private List<MantenimientosEntity>listaManteActivos;
+
+    public List<MantenimientosEntity> getListaManteActivos() {
+        return modelo.listarManteActivos();
+    }
+
+    public void setListaManteActivos(List<MantenimientosEntity> listaManteActivos) {
+        this.listaManteActivos = listaManteActivos;
+    }
 
     public String getIdRecurso() {
         return idRecurso;
@@ -40,17 +50,17 @@ public class MantenimientosBean {
         mante = new MantenimientosEntity();
 
   MantenimientosModel modelo = new MantenimientosModel();
- private MantenimientosEntity mante;
- private List<MantenimientosEntity> Listamante;
+ //private MantenimientosEntity mante;
+ //private List<MantenimientosEntity> Listamante;
  
- public MantenimientosBean() {
- mante = new MantenimientosEntity();
+ //public MantenimientosBean() {
+ //mante = new MantenimientosEntity();
  }
  public MantenimientosEntity getMantenimientos() {
  return mante;
  }
  public void setMantenimientos(MantenimientosEntity mante) {
-    public void setMantenimientos(MantenimientosEntity mante) {
+   // public void setMantenimientos(MantenimientosEntity mante) {
         this.mante = mante;
     }
 
@@ -61,28 +71,33 @@ public class MantenimientosBean {
     }
 
     public String guardarMantenimientos() {
+        //mante.setId(modelo.crearID());
         System.out.println( getIdRecurso() );
         mante.setIdRecurso(modelo.buscarRecursoId(getIdRecurso()));
         if (modelo.insertarMantenimientos(mante) != 1) {
             JsfUtil.setErrorMessage(null, "Ya se registró un mantenimiento con este ID");
-            return null;//Regreso a la misma página
+            return null;
         } else {
             JsfUtil.setFlashMessage("exito", "Mantenimiento registrado exitosamente");
             //Forzando la redirección en el cliente
-            return "TablaMantenimientos?faces-redirect=true";
+            return "/adminIT/mantenimiento/listar/listar?faces-redirect=true&result=1";
         }
     }
 
-    public String eliminarMantenimientos() {
-        // Leyendo el parametro enviado desde la vista
-        String id = JsfUtil.getRequest().getParameter("id");
-
-        if (modelo.eliminarMantenimientos(id) > 0) {
-            JsfUtil.setFlashMessage("exito", "Mantenimiento eliminado exitosamente");
-        } else {
-            JsfUtil.setErrorMessage(null, "No se pudo eliminar este Mantenimiento");
+    public String borrarMante(){
+    
+        String idRecurso= JsfUtil.getRequest().getParameter("id");
+        
+        if(modelo.eliminarMante(idRecurso) == 1){
+        
+            return "eliminarMante?faces-redirect=true&result=1";
+            
+        }else{
+        
+            return "eliminarMante?faces-redirect=true&result=0";
+            
         }
-        return "eliminarMante?faces-redirect=true";
+        
     }
 
     public String ModificarMantenimientos() {
